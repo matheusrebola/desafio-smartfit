cdbb158a86115f35a3e6f0ac243ae26346afa658
Matheus Cavalcanti Rebola <m.cavalcantir@outlook.com>
2023-10-09 16:57:54 -0300

    feat: configuração da component e acréscimo do módulo de formulários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FormsComponent } from './componentes/forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
